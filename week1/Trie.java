e68bfe2453e47282ad399639979f1b9ee6def4b7
Vivekanand Ganapathy Nagarajan <vivekn@gmail.com>
2016-08-02 14:35:20 -0700

    Implemented Trie data structure to store pattern of Strings

---
+++ week1/Trie.java
@@ -0,0 +1,96 @@
+package week1;
+import java.util.*;
+
+/**
+ * Trie class - implement the Trie data structure to store String pattern
+ *
+ * @author Vivekanand Ganapathy Nagarajan
+ * @version 1.0 August 2nd, 2016
+ */
+class Trie{
+
+    private TrieNode root;
+    private int counter;
+
+    Trie(String[] patterns){
+        root = new TrieNode(null);
+        buildTrie(patterns);
+    }
+
+    /**
+     * build a trie from a pattern of Strings
+     * @param patterns
+     */
+    private void buildTrie(String[] patterns) {
+        for (String pattern: patterns){
+            TrieNode trieNode = root;
+            for (int char_index=0; char_index < pattern.length(); char_index++){
+                Character c = pattern.charAt(char_index);
+                trieNode = insertTrie(c, trieNode);
+            }
+        }
+    }
+
+    /**
+     * print out contents of trie
+     */
+    void printoutTrie(){
+        printoutTrie(root);
+    }
+
+    /**
+     * Recursive helper to print out trie contents
+     * @param trieNode TrieNode
+     */
+    private void printoutTrie(TrieNode trieNode){
+        if (trieNode == null){
+            return;
+        }
+        for (Map.Entry<Character, TrieNode> child : trieNode.children.entrySet()) {
+                System.out.println(trieNode.val + "->" + child.getValue().val + ":" + child.getKey());
+                printoutTrie(child.getValue());
+        }
+    }
+
+    /**
+     * Insert character into trie node
+     * @param c character
+     * @param trieNode TrieNode
+     * @return TrieNode at which character was inserted
+     */
+    private TrieNode insertTrie(Character c, TrieNode trieNode){
+        if (trieNode.children.containsKey(c)){
+            return trieNode.children.get(c);
+        }
+        TrieNode newTrieNode = new TrieNode(c);
+        trieNode.children.put(c, newTrieNode);
+        return newTrieNode;
+    }
+
+    private class TrieNode {
+        Character c;
+        int val;
+        HashMap<Character, TrieNode> children;
+
+        TrieNode(Character c){
+            this.c = c;
+            children = new HashMap<Character, TrieNode>();
+            val = counter++;
+        }
+
+        public boolean equals( Object obj )
+        {
+            if (this == obj){
+                return true;
+            }
+            if (this.getClass() != obj.getClass()){
+                return false;
+            }
+            boolean flag = false;
+            TrieNode trieNode = (TrieNode)obj;
+            if( trieNode.c.equals(c))
+                flag = true;
+            return flag;
+        }
+    }
+}
---
+++ week1/TrieTest.java
@@ -0,0 +1,45 @@
+package week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+
+public class TrieTest {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    static public void main(String[] args) throws IOException {
+        new TrieTest().run();
+    }
+
+
+    void run() throws IOException {
+        FastScanner scanner = new FastScanner();
+        int patternsCount = scanner.nextInt();
+        String[] patterns = new String[patternsCount];
+        for (int i = 0; i < patternsCount; ++i) {
+            patterns[i] = scanner.next();
+        }
+        Trie trie = new Trie(patterns);
+        trie.printoutTrie();
+    }
+}
